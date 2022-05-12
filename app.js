3d49092b2ce1925925f01d2f155845085b6ede38
Laksh Mittal <mittallaksh1@gmail.com>
2022-05-12 21:14:30 +0530

    added secret manually and commented cookie parser

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.SECRET));
+// app.use(cookieParser(process.env.SECRET));
 
 app.use(
   session({
-    secret: process.env.SECRET,
+    secret: "myid",
     resave: false,
     saveUninitialized: false,
   })
